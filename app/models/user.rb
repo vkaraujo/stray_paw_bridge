4ec21283f02fcf1623d4ee8f0b335d1bafefebc4
viktor <araujo>
2025-04-13 19:38:36 -0300

    Devise and Pundit setup

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { user: 0, admin: 1, institutional: 2 }
+end
