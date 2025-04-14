5d88e836ce353a7a556c630f8dcbcf60788443aa
viktor <araujo>
2025-04-14 01:40:04 -0300

    chore: seed sample user and 3 pets for sanity testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,24 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(
-  email: 'admin@straypawbridge.test',
-  password: 'password',
-  password_confirmation: 'password',
-  role: :admin
-)
+User.find_or_create_by!(email: 'admin@straypawbridge.test') do |admin|
+  admin.password = 'password'
+  admin.role = :admin
+end
+
+user = User.find_or_create_by!(email: "sample@user.com") do |u|
+  u.password = "password"
+  u.role = :user
+end
+
+3.times do |i|
+  Pet.create!(
+    name: "Buddy ##{i + 1}",
+    description: "A friendly dog looking for a forever home.",
+    species: :dog,
+    size: %w[Small Medium Large].sample,
+    age: rand(1..5),
+    status: :available,
+    user: user
+  )
+end
